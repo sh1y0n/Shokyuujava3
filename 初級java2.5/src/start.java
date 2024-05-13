06ead6650a4da28886a94654a3dff0a65857ca78
sad <ads>
2024-05-13 11:05:59 +0900

    初級java2.5

---
+++ 初級java2.5/src/start.java
@@ -0,0 +1,21 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		
+        
+        MyFrame frame1 = new RectFrame();
+        frame1.setLocation(350,200);
+        
+        MyFrame frame2 = new RectFrame();
+        frame2.setLocation(50,300);
+        
+        int x,y;
+        x=2;
+        y=3;
+        
+        System.out.println(x*y);
+        
+	}
+
+}

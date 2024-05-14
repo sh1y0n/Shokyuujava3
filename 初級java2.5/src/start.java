2a241bc630943588476222f69c951b2286f0507b
sad <ads>
2024-05-14 10:44:43 +0900

    3.1更新
    5/14

--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -3,19 +3,9 @@ public class start {
 
 	public static void main(String[] args) {
 		
-        
-        MyFrame frame1 = new RectFrame();
-        frame1.setLocation(350,200);
-        
-        MyFrame frame2 = new RectFrame();
-        frame2.setLocation(50,300);
-        
-        int x,y;
-        x=2;
-        y=3;
-        
-        System.out.println(x*y);
-        
+        System.out.println("システムを実行します");
+        MyFrame framel =new RectTest();
+       
 	}
 
 }

f8cf46772269cf74619fce4467a3663e72328251
sad <ads>
2024-05-14 12:01:35 +0900

    3.3更新

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -1,9 +1,21 @@
 
 public class RectTest extends MyFrame  {
 public void run(){
+	setColor(255,255,0);
 	fillRect(30,40,50,100); //extendsは継承らしい　myframeとつながってる的な
+	
+	setColor(200,0,255);
 	fillRect(130,40,50,100);
+	
+	setColor(0,255,255);
 	fillRect(30,200,150,10);
-	fillRect(90,160,30,30);
+	
+	setColor(0,0,0);
+	fillRect(200,0,100,100);
+	
+	setColor(50,50,55);
+	fillRect(30,40,500,10);
+	
+	
 }
 }

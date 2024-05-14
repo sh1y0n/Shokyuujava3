6793a81f92bbdd5098cf4f5b76dfa0f85d12599d
sad <ads>
2024-05-14 12:18:19 +0900

    3.4更新

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -1,20 +1,22 @@
 
 public class RectTest extends MyFrame  {
 public void run(){
+	
+	//yellow
 	setColor(255,255,0);
 	fillRect(30,40,50,100); //extendsは継承らしい　myframeとつながってる的な
-	
+	//parple
 	setColor(200,0,255);
 	fillRect(130,40,50,100);
-	
+	//watercoler
 	setColor(0,255,255);
 	fillRect(30,200,150,10);
-	
+	//black
 	setColor(0,0,0);
-	fillRect(200,0,100,100);
-	
+	fillRect(200,200,100,100);
+	//haiiro
 	setColor(50,50,55);
-	fillRect(30,40,500,10);
+	fillRect(300,300,100,100);
 	
 	
 }
---
+++ 初級java2.5/src/ovalTest.java
@@ -0,0 +1,9 @@
+
+public class ovalTest extends MyFrame {
+	public void run() {
+		fillOval (30,40,50,100);
+		fillOval (100,40,50,100);
+		fillRect(100,100,100,100);
+	}
+
+}

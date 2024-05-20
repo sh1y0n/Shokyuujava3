9f3f34538c0300ba581abf1baa67d795d68ffde7
sad <ads>
2024-05-20 10:20:13 +0900

    練習問題3-1

--- 初級java2.5/src/ovalTest.java
+++ 初級java2.5/src/ovalTest.java
@@ -3,7 +3,7 @@ public class ovalTest extends MyFrame {
 	public void run() {
 		fillOval (30,40,50,100);
 		fillOval (100,40,50,100);
-		fillRect(100,100,100,100);
+		fillRect(10,200,150,10);
 	}
 
 }

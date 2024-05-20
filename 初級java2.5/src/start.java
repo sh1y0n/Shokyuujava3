cff3a9859f3e1dbcbf7fc1017c6163b7e72d5ccd
sad <ads>
2024-05-20 11:28:22 +0900

    ４－２もんだい

--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -3,7 +3,7 @@ public class start {
 
 	public static void main(String[] args) {
 		
-        System.out.println("システムを実行しますz");
+        System.out.println("システムを実行します");
         MyFrame framel =new RecrLoop();
         
                

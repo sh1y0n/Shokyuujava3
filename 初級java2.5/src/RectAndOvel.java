eef0af7da529e45519a3405c98ba23c87d6c0cfe
sad <ads>
2024-05-20 11:14:33 +0900

    Revert "RectLoop"
    This reverts commit 236c8800f215d8f3991501027271d764723f323d.

--- 初級java2.5/src/RectAndOvel.java
+++ 初級java2.5/src/RectAndOvel.java
@@ -4,6 +4,6 @@ public void run() {
 	fillRect(100,100,100,50);
 	fillRect(125,80,50,25);
 	fillOval(110,140,25,25);
-	fillOval(165,140,25,25); 
+	fillOval(165,140,25,25);
 }
 }
--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -4,7 +4,7 @@ public class start {
 	public static void main(String[] args) {
 		
         System.out.println("システムを実行しますz");
-        MyFrame framel =new RecrLoop();
+        MyFrame framel =new ManyFigures();
         
                
 	}

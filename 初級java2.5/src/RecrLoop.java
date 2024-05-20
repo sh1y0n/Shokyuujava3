d006195fb39d808eabefc37a22922fca6c12c6be
sad <ads>
2024-05-20 12:32:00 +0900

    4-3

--- 初級java2.5/src/RecrLoop.java
+++ 初級java2.5/src/RecrLoop.java
@@ -1,14 +1,14 @@
 
 public class RecrLoop extends MyFrame {
 public void run(){
-setColor(0,128,0);
+
 int i,x=30;
 for(i=0;i<9;i++)
 {
 	
-		fillRect(x,30,10,x);
+		fillRect(x,30,10,100);
 		x+=30;
-
+		setColor(x,x,x);
 	
 }
 }

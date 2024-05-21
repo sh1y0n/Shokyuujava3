97ae49e10e2a0e88ebde62d5a0a72f005d360717
sad <ads>
2024-05-21 11:57:49 +0900

    5-1 確認問題

--- 初級java2.5/src/DisplayRectSlow.java
+++ 初級java2.5/src/DisplayRectSlow.java
@@ -2,10 +2,11 @@
 public class DisplayRectSlow extends MyFrame {
 	public void run() {
 		setColor(0,128,0);
-		int x=30;
+		int x=30,y=80;
 		while(x<=170) {
-				fillRect(x,80,10,100);
+				fillRect(x,y,10,100);
 				x+=20;
+				y+=20;
 				sleep(1);
 		}
 	}

01a50a5c4acb39853aab23ee054b26ea58a8c0f8
sad <ads>
2024-05-21 11:12:05 +0900

    追加問題②を追加提出

--- 初級java2.5/src/RecrLoop.java
+++ 初級java2.5/src/RecrLoop.java
@@ -2,16 +2,15 @@
 public class RecrLoop extends MyFrame {
 public void run(){
 
-int i,x=20,z=20;
+int i,x=20,z=200,y=20;
 for(i=0;i<9;i++)
 {
 	
-		fillRect(x,x,z,z);	
-		x=x+z;
-		z=z+10;
-		
-		
-
+		fillRect(x,0,10,z);	
+		fillRect(200-x,300-y,10,z);
+		x+=20;
+		z-=10;
+		y-=10;
 	
 	}
 	}     																}	

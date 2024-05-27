836d78e352073c21d95e9d8931d062fe163408e6
sad <ads>
2024-05-27 11:01:11 +0900

    練習５－２

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -1,26 +1,38 @@
 
 public class Animation extends MyFrame {
 public void run() {
-	int y=100,x=30;
+	int y=30,x=30;
 	
 	
-	while (y>=30) {
+	while (y<=150) {
 		//(1画面を消す)
 		clear();
 		//(2)四角形を表示する
 		setColor(0,128,0);
 		fillRect(x,y,10,100);
-		y-=5;
+		x+=5;
+		y+=5;
 		sleep(0.1);
 		
 	}
+	
 	while(x>=30) {
 		clear();
 		setColor(0,128,0);
 		fillRect(x,y,10,100);
-		x+=5;
+		x-=5;
 		sleep(0.1);
 	}
-	
+	while (true) {
+		//(1画面を消す)
+		clear();
+		//(2)四角形を表示する
+		setColor(0,128,0);
+		fillRect(x,y,10,100);
+		x+=5;
+		y-=5;
+		sleep(0.1);
+		
+	}
 }
 }

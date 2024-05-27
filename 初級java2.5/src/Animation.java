ebf5667f17c116f8740a46938b975fefdd3cb275
sad <ads>
2024-05-27 10:02:45 +0900

    ５．６

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -2,14 +2,18 @@
 public class Animation extends MyFrame {
 public void run() {
 	int x=30;
-	while (x<=170) {
+	while (true) {
 		//(1画面を消す)
 		clear();
 		//(2)四角形を表示する
 		setColor(0,128,0);
 		fillRect(x,80,10,100);
-		x+=20;
-		sleep(1);
+		
+		if(x >200) { //xが200より大きくなったらwhileの繰り返しをやめる
+			break;
+		}
+		x+=5;
+		sleep(0.1);
 	}
 }
 }

5d9597930c8bc86beed2f847dbf7a1f8bfe3235d
sad <ads>
2024-05-28 11:54:41 +0900

    7-2

--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -4,7 +4,7 @@ public class start {
 	public static void main(String[] args) {
 		
         System.out.println("システムを実行します");
-        MyFrame framel =new DisplayFaceAndTree();
+        MyFrame framel =new DisplayFaceObject();
         
                
 	}

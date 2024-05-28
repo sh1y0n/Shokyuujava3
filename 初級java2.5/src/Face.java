4270889ea22c9a58fcef037845edc853a363b25c
sad <ads>
2024-05-28 10:06:17 +0900

    classがむずいー

--- 初級java2.5/src/Face.java
+++ 初級java2.5/src/Face.java
@@ -1,9 +1,15 @@
 
 public class Face {
 int x,y;
-public void draw(MyFrame frame) {
-	frame.fillOval(x, y, 50, 100);
+public Face(int x,int y) {
+	this.x=x;
+	this.y=y;
+	
+	
+}
+public void draw (MyFrame frame) {
+	frame.fillOval(x,y,50,100);
 	frame.fillOval(x+100, y, 50, 100);
-	frame.fillRect(x,y+160,150,10);
+	frame.fillRect(x, y+160, 150, 10);
 }
 }

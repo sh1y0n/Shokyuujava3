19009f599b6d4611ba1326ce23cddbf6b85791d2
sad <ads>
2024-05-28 09:57:56 +0900

    kclass

---
+++ 初級java2.5/src/DisplayFace.java
@@ -0,0 +1,17 @@
+
+public class DisplayFace extends MyFrame {
+public void run() {
+int x=10,y=40;
+	
+fillOval(x,y,50,100);
+fillOval(x+100,y,50,100);
+fillRect(x,y+160,150,10);
+
+int x1=180,y1=120;
+
+fillOval(x1,y1,50,100);
+fillOval(x1+100,y1,50,100);
+fillRect(x1,y1+160,150,10);
+	}
+
+}

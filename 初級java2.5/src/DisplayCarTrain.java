d7ab5809abc4ea3b7e036d8f54cf9a922a22b125
sad <ads>
2024-06-10 10:41:55 +0900

    8-3 keisyou muzui

---
+++ 初級java2.5/src/DisplayCarTrain.java
@@ -0,0 +1,10 @@
+
+public class DisplayCarTrain extends MyFrame {
+public void run() {
+	Car carl =new Car(10,50,3,0);
+			carl.draw(this);
+	Train trainl =new Train(30,150,3,0);
+	trainl.draw(this);
+			
+}
+}
